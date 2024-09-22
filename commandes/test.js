fc3098a96de9f87af25a60146b0ff5b8e507bc15
JUSTATZ-MD <justatz04@gmail.com>
2024-09-22 14:37:36 +0300

    Update test.js

--- commandes/test.js
+++ commandes/test.js
@@ -6,7 +6,7 @@ zokou({ nomCom: "justatz", reaction: "nipobos", nomFichier: __filename }, async 
     let z = 'Hello my name is  *JUSTATZ md  * \n\n ' + "i'm a whatsapp bot multi-device created ";
     let d = ' by *JUSTATZ Tech*';
     let varmess = z + d;
-    var img = 'https://telegra.ph/file/347ba7a613b4d025b89a8.jpg';
+    var img = 'https://i.imgur.com/8nyhO23.jpeg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
     //console.log("montest")
 });
